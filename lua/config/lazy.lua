17510737b7bf42910f3cd86e7925a058ed7a2d5b
42edwa <eduaugusto@proton.me>
2026-08-19 19:32:10 -0300

    Check for plugin updates once a week

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,5 +31,11 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+
+  -- automatically check for plugin updates
+	checker = {
+	  enabled = true,
+	  notify = false,
+	  frequency = 604800, -- 7 days
+	},
 })
